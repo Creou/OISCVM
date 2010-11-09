5f9e92a270e335df026409f3cdd2a1282eb8fcd0
Simon P Stevens <simonpstevens@gmail.com>
2010-11-09 19:20:18 +0000

    Added file loading and initial line processing to instructions.

--- OISC_Compiler/Program.cs
+++ OISC_Compiler/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace OISC_Compiler
 {
@@ -9,9 +10,40 @@ namespace OISC_Compiler
     {
         static void Main(string[] args)
         {
-            DisplayUsage();
+            if (args.Length != 1)
+            {
+                DisplayUsage(); 
+                return;
+            }
 
-            Console.ReadLine();
+            String filePath = args[0];
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("{0} not found", filePath);
+                return;
+            }
+
+            // Load the source.
+            Console.WriteLine(Strings.AppName);
+            Console.WriteLine();
+            Console.WriteLine("Loading source ({0})", filePath);
+            String[] sourceCodeLines = File.ReadAllLines(filePath);
+
+            // Compile source.
+            OISCCompiler compiler = new OISCCompiler(sourceCodeLines);
+            byte[] compiledBytes = compiler.Compile();
+
+            // Output compiled binary to file.
+            String outputFilePath = GenerateOutputFilename(filePath);
+            File.WriteAllBytes(outputFilePath, compiledBytes);
+        }
+
+        private static String GenerateOutputFilename(String inputFileName)
+        {
+            String nameWithoutExtension = Path.GetFileNameWithoutExtension(inputFileName);
+            String path = Path.GetDirectoryName(inputFileName);
+
+            return Path.Combine(path, nameWithoutExtension + ".bin");
         }
 
         private static void DisplayUsage()
