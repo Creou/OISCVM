4467cf2fb29e75103692250d67af0a90136cdb58
Simon P Stevens <simonpstevens@gmail.com>
2010-11-09 19:38:21 +0000

    Added files missing from last commit.(See previous message)

---
+++ OISC_Compiler/Instruction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OISC_Compiler
+{
+    public enum InstructionType
+    {
+        Comment = 0,
+        SUBLEQ = 1
+    }
+
+    public abstract class Instruction
+    {
+        public String SourceLine{get;private set;}
+
+        public Instruction(String sourceLine)
+        {
+            this.SourceLine = sourceLine;
+        }
+
+    }
+
+    public class CommentInstruction : Instruction
+    {
+        public CommentInstruction(String sourceLine)
+            : base(sourceLine)
+        {
+        }
+    }
+
+    public class SubleqInstruction : Instruction
+    {
+        public String Operand_a { get; private set; }
+        public String Operand_b { get; private set; }
+        public String Opernad_c { get; private set; }
+
+        public SubleqInstruction(String sourceLine, String operand_a, String operand_b, String operand_c)
+            : base(sourceLine)
+        {
+            this.Operand_a = operand_a;
+            this.Operand_b = operand_b;
+            this.Opernad_c = operand_c;
+        }
+    }
+}
---
+++ OISC_Compiler/OISCCompiler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OISC_Compiler
+{
+    public interface ICompiler
+    {
+        byte[] Compile();
+    }
+
+    public class OISCCompiler :ICompiler
+    {
+        private String[] _sourceCodeLines;
+
+        public OISCCompiler(String[] sourceCodeLines)
+        {
+            _sourceCodeLines = sourceCodeLines;
+        }
+
+        public byte[] Compile()
+        {
+            List<Instruction> instructionList = new List<Instruction>();
+            foreach (String sourceLine in _sourceCodeLines)
+            {
+                Instruction sourceInstruction = GenerateInstruction(sourceLine);
+                instructionList.Add(sourceInstruction);
+            }
+
+            return new byte[1];
+        }
+
+        private Instruction GenerateInstruction(String sourceLine)
+        {
+            if (sourceLine.StartsWith("//"))
+            {
+                // This line is a comment.
+                return new CommentInstruction(sourceLine);
+            }
+            else 
+            {
+                String[] instructionDate = sourceLine.Split(' ');
+                String operand_a = instructionDate[0];
+                String operand_b = instructionDate[1];
+                String operand_c = instructionDate[2];
+                return new SubleqInstruction(sourceLine, operand_a, operand_b, operand_c);
+            }
+        }
+    }
+}
