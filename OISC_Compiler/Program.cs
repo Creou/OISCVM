d149658e3e2280ec504856a6a154a72b38212bfa
Simon P Stevens <simonpstevens@gmail.com>
2010-11-09 19:16:11 +0000

    Created new compiler project

---
+++ OISC_Compiler/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OISC_Compiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayUsage();
+
+            Console.ReadLine();
+        }
+
+        private static void DisplayUsage()
+        {
+            Console.WriteLine(Strings.AppName);
+            Console.WriteLine();
+            Console.WriteLine(Strings.Usage);
+        }
+    }
+}
